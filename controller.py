b7f0e9d0629ad7608590652f6db39bc4246f2c8d
Jacob Hortman <jacob.hortman@gmail.com>
2023-04-24 23:11:07 -0500

    Renamed the assignment 2 file to follow MVC format. Changed functions to align with GUI buttons. Added TODOs to the file.

---
+++ controller.py
@@ -0,0 +1,39 @@
+def enter_scores(app):
+    input_scores = []
+    number_of_students = int(app.student_number_input)
+    input_scores.append(float(app.student_score_input))
+
+    # TODO: Display the scores in the table widget
+
+    app.student_score_input.clear()
+
+    # TODO: Add exception handling
+
+
+def clear_entries(app):
+    app.student_number_input.clear()
+    app.student_score_input.clear()
+
+    # TODO: Clear the table widget
+
+
+def calculate_grades(app, scores):
+    # TODO: Find a way to input the scores list from the enter_scores() method
+    final_grades = []
+    best_score = max(scores)
+
+    for x in range(0, len(scores)):
+        if scores[x] >= (best_score - 10):
+            final_grades.append('A')
+        elif scores[x] >= (best_score - 20):
+            final_grades.append('B')
+        elif scores[x] >= (best_score - 30):
+            final_grades.append('C')
+        elif scores[x] >= (best_score - 40):
+            final_grades.append('D')
+        else:
+            final_grades.append('F')
+
+    # TODO: Display the grades on the table widget
+
+    # TODO: Add exception handling
